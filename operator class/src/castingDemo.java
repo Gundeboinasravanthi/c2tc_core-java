102f46b7dccc9b9760ed71aa7198fcbc5fd315e6
Sravanthi <Sravanthi@DESKTOP-4JNAVD7>
2023-10-04 22:29:16 +0530

    firstcommit

---
+++ operator class/src/castingDemo.java
@@ -0,0 +1,15 @@
+
+public class castingDemo {
+
+	public static void main(String[] args) {
+		byte b =10; 
+		int i=b;
+		System.out.println(i);
+float f=22.14f;
+double d =f;
+System.out.println(d);
+           char ch='A';
+           int j
+	}
+
+}
---
+++ operator class/src/operatorsDemo.java
@@ -0,0 +1,24 @@
+
+public class operatorsDemo {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		++a;
+		int c = ++a + b + a--;
+		System.out.println(c);
+a=11 ; b=20 ; c=44 ; 
+int d=c++ + a + b--;
+System.out.println(d);
+System.out.println(c);
+System.out.println(b);
+    
+    
+    
+ 
+    		
+	}
+
+}
+;
